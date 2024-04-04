e050396feb5416fcc306023f78d150ef1e5a9173
d-asnaghi <30296575+hexdae@users.noreply.github.com>
2024-04-04 17:22:42 -0500

    [RELEASE] update notes

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -8,18 +8,15 @@ TAG=${GITHUB_REF_NAME}
 # The prefix is chosen to match what GitHub generates for source archives
 # This guarantees that users can easily switch from a released artifact to a source archive
 # with minimal differences in their code (e.g. strip_prefix remains the same)
-PREFIX="/toolchains_riscv_gnu-${TAG:1}"
-ARCHIVE="/toolchains_riscv_gnu-$TAG.tar.gz"
+PREFIX="/toolchains_riscv_gnu_${TAG:1}"
+ARCHIVE="/toolchains_riscv_gnu_$TAG.tar.gz"
 
 # NB: configuration for 'git archive' is in /.gitattributes
 git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
 SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
 
 cat << EOF
-## Using Bzlmod with Bazel 6 or greater
-
-1. (Bazel 6 only) Enable with \`common --enable_bzlmod\` in \`.bazelrc\`.
-2. Add to your \`MODULE.bazel\` file:
+## MODULE.bazel
 
 \`\`\`starlark
 bazel_dep(name = "toolchains_riscv_gnu", version = "${TAG:1}")
@@ -39,4 +36,28 @@ use_repo(
 
 register_toolchains("@riscv_none_elf//toolchain:all")
 \`\`\`
+
+## WORKSPACE
+
+\`\`\`starlark
+load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+http_archive(
+    name = "rules_cc",
+    sha256 = "2037875b9a4456dce4a79d112a8ae885bbc4aad968e6587dca6e64f3a0900cdf",
+    strip_prefix = "rules_cc-0.0.9",
+    urls = ["https://github.com/bazelbuild/rules_cc/releases/download/0.0.9/rules_cc-0.0.9.tar.gz"],
+)
+
+load("@bazel_tools//tools/build_defs/repo:git.bzl", "git_repository")
+git_repository(
+    name = "toolchains_riscv_gnu",
+    remote = "https://github.com/hexdae/toolchains_riscv_gnu",
+    tag = "${TAG}",
+)
+
+load("@toolchains_riscv_gnu//:deps.bzl", "riscv_none_elf_deps")
+riscv_none_elf_deps()
+register_toolchains("@riscv_none_elf//toolchain:all")
+\`\`\`
+
 EOF
